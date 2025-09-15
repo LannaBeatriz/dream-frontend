f07c33130ebf13e3a27a0a78917a4458f0beb337
LannaBeatriz <00001085525120sp@al.educacao.sp.gov.br>
2025-09-15 20:25:51 -0300

    hello

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 // Supondo que você tenha um componente para a página de metas
-// import Metas from './pages/Metas.jsx'; 
+import MetasPage from './components/MetasPage.jsx'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        {/* <Route path="/metas" element={<Metas />} /> */}
+        <Route path="/metas" element={<MetasPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
